e1d386ea6de68627dd5e709ad4c88ecf5874fd9d
Jules Kreuer <contact@juleskreuer.eu>
2022-07-07 21:46:54 +0200

    update date for next event

--- config.php
+++ config.php
@@ -7,20 +7,20 @@ $CRED_DBNAME = "";
 /////////////////////////////////////////////////////////////////////////////////////////////////
 $CONTACT_EMAIL = "pizza@juleskreuer.eu";
 
-$DATE = "04.07.2022 11-14 Uhr"; // Date/Time of the event. (Human readable)
+$DATE = "21.07.2022 11-14 Uhr"; // Date/Time of the event. (Human readable)
 $LOCATION = "Sand Kitchen A104"; // Location of the event.
-$BASE_PRICE = 2.5;   // in € 
+$BASE_PRICE = 3;   // in € 
 
 // Time of the event //   Hour  Min  Sec  Month Day   Year
-$TIMESTAMP_START = mktime('11', '0', '0', '07', '04', '2022');
-$TIMESTAMP_STOP =  mktime('14', '0', '0', '07', '04', '2022');
-$SLOTS = 24;         // Number of order-slots: 3h, 18 Slots -> 10min per slot
+$TIMESTAMP_START = mktime('11', '0', '0', '21', '04', '2022');
+$TIMESTAMP_STOP =  mktime('14', '0', '0', '21', '04', '2022');
+$SLOTS = 24;         // Number of order-slots: 3h, 24 Slots -> 7.5 min per slot
 
 // Time of the last possible order
-$TIMESTAMP_LAST_ORDER = mktime('6', '0', '0', '07', '04', '2022');
+$TIMESTAMP_LAST_ORDER = mktime('15', '0', '0', '20', '04', '2022');
 
-$PIZZA_TIME = 5; // Time needed for one pizza in minutes.
-$ADDITIONAL_TIME = 2.5; // time needed for any additional pizza.
+$PIZZA_TIME = 4; // Time needed for one pizza in minutes.
+$ADDITIONAL_TIME = 1; // time needed for any additional pizza. -> 4 Pizza per slot
 $BLOCK_OVERLAPPING_ORDERS = True; // Do not allow orders if there are too many others
 $MAX_NUMBER_INGREDIENTS = 6; // Maximum number of ingredients.
 
